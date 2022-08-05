a6eea359a888fa950dac442d021e4285f805d6eb
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-08-05 14:32:06 -0300

    csoas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="navbar navbar-expand-lg navbar-light bg-light d-flex flex-row bd-highlight mt-5">
             <a className="ms-5 me-5" href="http://instagram.com">instagram</a>
             <div className="me-5">legales lmao</div>
-            <div className="me-5">copyright valentin</div>
+            <div className="me-5">copyright valentin </div>
         </div>
     );
 }
